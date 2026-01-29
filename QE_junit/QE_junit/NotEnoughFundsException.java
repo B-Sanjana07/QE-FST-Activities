4cbc2a7d5ff8e9bf0c4d7d3548d731f5c148ee99
B-Sanjana07 <sanjanab1310@gmail.com>
2026-01-29 18:15:02 +0530

    Delete QE_junit/QE_junit directory

--- QE_junit/QE_junit/NotEnoughFundsException.java
+++
@@ -1,7 +0,0 @@
-public class NotEnoughFundsException extends RuntimeException {
-
-    public NotEnoughFundsException(Integer amount, Integer balance) {
-        super("Attempted to withdraw " + amount + " with balance of " + balance);
-    }
-}
-
